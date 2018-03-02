4e94748de68b1ead5f689cd9f7195e8269e08012
dminGod <dmindesigns@gmail.com>
2018-03-02 11:17:15 +0700

    Postgres push added

--- postgres.go
+++ postgres.go
@@ -1,4 +1,440 @@
 package main
 
+import (
+	"github.com/bsm/sarama-cluster"
+	"fmt"
+	//"github.com/linkedin/goavro"
 
-	// TODO : Write logic to make inserts into Postgres-XL as well.
+	//"io/ioutil"
+	"github.com/antigloss/go/logger"
+	"errors"
+	"os"
+	"os/signal"
+	//"strings"
+	//"time"
+	//"github.com/gocql/gocql"
+	"encoding/json"
+	"github.com/golang/snappy"
+	"strings"
+	"database/sql"
+	_ "github.com/lib/pq"
+	//"github.com/Shopify/sarama"
+	"github.com/Shopify/sarama"
+	"time"
+	"math/rand"
+)
+
+//var dbHost, dbPort, dbUser, dbPass, dbName string
+var dbpool []*sql.DB
+
+
+
+// TODO : Write logic to make inserts into Postgres-XL as well.
+
+func PostgresPush(){
+
+	bl := Conf.Kafka.KafkaBrokers
+	topicName := Conf.Kafka.TopicsListForThisNode
+
+
+	config := cluster.NewConfig()
+	config.Consumer.Return.Errors = true
+	config.Group.Return.Notifications = true
+
+	//schemaFile := Conf.Kafka.SchemaFile
+
+	//schemaFileLocation = strings.TrimRight(schemaFileLocation, "/")
+
+	//b, err := ioutil.ReadFile(fmt.Sprintf("%v",schemaFile))
+	//if err != nil {
+	//	logger.Error("There was an error in opening the schema file",err)
+	//}
+	//codec, err = goavro.NewCodec(fmt.Sprintf(string(b), topicName))
+
+	if len(bl) == 0 {
+
+		//logger.Error("No brokers specified, exiting")
+		e := "No brokers specified, can not start the Kafka listener"
+		logger.Error(e)
+		//err := errors.New(e)
+		return
+	}
+
+	consumer, err := cluster.NewConsumer(bl, "HectorBGWorkersHDFSPush", []string{topicName}, config)
+	if err != nil {
+		s := fmt.Sprintf("Unable to make consumer, Broker list : %v, Topics_list : %v, Config : %v, got error : %v", bl, topicName, config, err)
+		logger.Error(s)
+		err = errors.New(s)
+		return
+	}
+
+	defer consumer.Close()
+
+	// trap SIGINT to trigger a shutdown.
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, os.Interrupt)
+
+	//consume errors
+	go func() {
+		for err := range consumer.Errors() {
+			logger.Error("Conusmer error : %s ", err.Error())
+			return
+		}
+	}()
+
+	// consume notifications
+	go func() {
+		for ntf := range consumer.Notifications() {
+			logger.Error("Kafka notification : Rebalanced: %+v ", ntf)
+		}
+	}()
+
+
+
+
+	//fmt.Printf("Trying to connect")
+	//connection, err := sql.Open("postgres", dbInfo)
+	//if err != nil {
+	//
+	//	fmt.Println("There was an error with the connction", err)
+	//	os.Exit(1)
+	//}
+
+	connection, errdb := getConnection()
+
+	if errdb != nil {
+
+		logger.Error(fmt.Sprintf(" ErrorType : INFRA_ERROR, Unable to reach any Postgres servers! PANIC, Error : %v", errdb.Error()))
+		os.Exit(1)
+
+	}
+
+
+ConsumerLoop:
+	for {
+		select {
+		case msg, ok := <-consumer.Messages():
+
+
+			if ok {
+
+
+
+				//sdc := msg.Value
+				//sdc, err := snappy.Decode(nil, msg.Value)
+				////s := []byte(fmt.Sprintf(`%v`,sdc))
+				//if err != nil {
+				//
+				//	logger.Error("Error decoding snappy message, Error : %v", err)
+				//	os.Exit(1)
+				//}
+				//fmt.Printf("there was a message")
+				q := GenerateQuery(msg)
+
+				//fmt.Printf("the generated query is %v", q)
+
+				_, err = connection.Exec(q)
+				if err != nil {
+
+					logger.Error("There was an error with executing this query", err)
+					os.Exit(1)
+				}
+
+
+				connection.Close()
+
+
+				consumer.MarkOffset(msg, "")
+			} else {
+
+				logger.Error("Problem with message that came in -- Channel says ok is false")
+			}
+
+		case <-signals:
+			break ConsumerLoop
+		}
+	}
+
+}
+
+func GenerateQuery(msg *sarama.ConsumerMessage) (q string){
+	//func GenerateQuery() (q string){
+	//fmt.Printf("came here")
+	var tbName = "local_service_requests_new_con5"
+	sdc := msg.Value
+	sdc, err := snappy.Decode(nil, sdc)
+	if err != nil {
+
+		logger.Error("Error decoding snappy message, Error : %v", err)
+		os.Exit(1)
+	}
+
+	var k interface{}
+
+	err = json.Unmarshal(sdc, &k)
+	if err != nil {
+
+		logger.Error("Error converting JSON:", err)
+		os.Exit(1)
+	}
+
+	var keys, vals string
+
+
+	if _, ok := k.(map[string]interface{}); ok {
+
+		l := k.(map[string]interface{})
+
+		for key, v := range l {
+
+			switch vv := v.(type) {
+
+			case string:
+				if key == "local_service_requests_s"{
+					keys += fmt.Sprintf("%v,", key)
+					vals += fmt.Sprintf("'{%v}',", vv)
+					continue
+				}
+				keys += fmt.Sprintf("%v,", key)
+				vals += fmt.Sprintf("'%v',", vv)
+				//fmt.Printf("this is string %v\n", key)
+
+			case []interface{}:
+				//fmt.Printf("array of interface for %v\n", key)
+				var tmpArr []string
+
+				for _, vvv := range vv {
+
+					switch vvv.(type) {
+
+					case string:
+						tmpArr = append(tmpArr, vvv.(string))
+					}
+				}
+
+				if len(tmpArr) > 0 {
+					keys += fmt.Sprintf("%v,", key)
+					tmpVals := fmt.Sprintf("'%v'", strings.Join(tmpArr, "','"))
+					vals += fmt.Sprintf("ARRAY[%v]::text[],", tmpVals)
+				}
+
+
+			case int64:
+				//fmt.Printf("this is int64 %v\n",key)
+				keys += fmt.Sprintf("%v,", key)
+				vals += fmt.Sprintf("%v,", vv)
+
+
+			case int:
+				//fmt.Printf("this is int %v\n", key)
+				keys += fmt.Sprintf("%v,", key)
+				vals += fmt.Sprintf("%v,", vv)
+
+			case float64:
+				//fmt.Printf("this is float64 %v\n",key)
+				if key == "int_created_date_cass" { continue }
+				if key == "int_created_date_cas" { continue }
+
+				if key == "int_created_date" {
+
+					dt := int64((vv / 1000) * 1000)
+
+					keys += fmt.Sprintf("%v,", "int_created_date")
+					vals += fmt.Sprintf("to_timestamp(%v),", dt)
+
+					continue
+				}
+
+				if key == "int_updated_date" {
+					dt := int64((vv / 1000) * 1000)
+
+					keys += fmt.Sprintf("%v,", "int_updated_date")
+					vals += fmt.Sprintf("to_timestamp(%v),", dt)
+
+					continue
+				}
+				if key == "order_date" {
+					dt := int64((vv / 1000) * 1000)
+
+					keys += fmt.Sprintf("%v,", "order_date")
+					vals += fmt.Sprintf("to_timestamp(%v),", dt)
+
+					continue
+				}
+
+				if key == "submitted_date" {
+					dt := int64((vv / 1000) * 1000)
+
+					keys += fmt.Sprintf("%v,", "submitted_date")
+					vals += fmt.Sprintf("to_timestamp(%v),", dt)
+
+					continue
+				}
+
+				if key == "order_info_error_date_new" {
+					dt := int64((vv / 1000) * 1000)
+
+					keys += fmt.Sprintf("%v,", "order_info_error_date_new")
+					vals += fmt.Sprintf("to_timestamp(%v),", dt)
+
+					continue
+				}
+
+				if key == "order_info_error_date_time" {
+					dt := int64((vv / 1000) * 1000)
+
+					keys += fmt.Sprintf("%v,", "order_info_error_date_time")
+					vals += fmt.Sprintf("to_timestamp(%v),", dt)
+
+					continue
+				}
+
+				if key == "order_info_error_date" {
+					dt := int64((vv / 1000) * 1000)
+
+					keys += fmt.Sprintf("%v,", "order_info_error_date")
+					vals += fmt.Sprintf("to_timestamp(%v),", dt)
+
+					continue
+				}
+
+
+
+
+				dt := int64((vv / 1000) * 1000)
+				keys += fmt.Sprintf("%v,", key)
+
+				if dt > 1000000 {
+
+					vals += fmt.Sprintf("to_timestamp(%v),", dt)
+				} else {
+
+					vals += fmt.Sprintf("%v,", dt)
+				}
+
+			default:
+				fmt.Printf("`Field : %v, Value : %v, Type : `\n", key, vv)
+
+			}
+		}
+
+		keys = strings.TrimRight(keys, ",")
+		vals = strings.TrimRight(vals, ",")
+
+
+		q = fmt.Sprintf("INSERT INTO %v (%v) VALUES (%v);", tbName, keys, vals)
+		//os.Stdout.Write([]byte(q))
+
+
+	} else {
+
+		logger.Error("Expecting data as map[string]interface{} got : ", k)
+		os.Exit(1)
+	}
+
+	return
+
+}
+
+
+func getConnection() (*sql.DB, error) {
+
+	checkCount := 0
+
+	//	Conf := config.Get()
+
+getNew:
+	if len(dbpool) > 0 {
+
+		randNum := rand.Intn(len(dbpool))
+
+		if dbpool[randNum].Ping() != nil {
+
+			if checkCount < 10 {
+
+				logger.Warn(fmt.Sprintf("Not able to ping server number %v for postgres connection, please remove this from config.", randNum))
+
+				checkCount += 1
+
+				goto getNew
+			} else {
+
+				logger.Error("ErrorType : INFRA_ERROR, Tried 9 times not able to get connection from pool, giving up. Too many tries to server")
+				return nil, errors.New("Tried 9 times not able to get connection from pool, giving up. Too many tries to server")
+			}
+		} else {
+
+			//if dbAbstract.Quiet == false {
+			//
+				logger.Info(fmt.Sprintf("Returning connection : %v , Total Size: %v , Connections : %v , Stats : %v", randNum, len(dbpool), dbpool, dbpool[randNum].Stats()))
+			//}
+			//
+			return dbpool[randNum], nil
+		}
+	} else {
+
+		logger.Error("ErrorType : INFRA_ERROR, Unable to reach any Postgres servers! PANIC, Trying to reload")
+		LoadPostgres()
+		goto getNew
+	}
+
+	return nil, errors.New("Error in establishing connection")
+}
+
+
+
+func LoadPostgres() {
+
+	//time.Sleep(2 * time.Second)
+	//Conf := app_config.Get()
+
+	dbName := Conf.Postgresxl.Database
+	dbUser := Conf.Postgresxl.Username
+	dbPass := Conf.Postgresxl.Password
+
+	rand.Seed(time.Now().UTC().UnixNano())
+
+	for _, curDB := range Conf.Postgresxl.Host {
+
+		// Get the host and port
+		curDBSplit := strings.Split(curDB, ":")
+
+		if len(curDBSplit) == 1 {
+
+			logger.Error(fmt.Sprintf("ErrorType : CONFIG_ERROR, Got server configured without port information skipping server, Server: %v", curDB))
+			continue
+		}
+
+		dbHost := curDBSplit[0]
+		dbPort := curDBSplit[1]
+
+		var dbInfo string
+
+		if len(dbPass) == 0 {
+
+			dbInfo = fmt.Sprintf("user=%s dbname=%s sslmode=disable host=%s port=%s",
+				dbUser, dbName, dbHost, dbPort)
+		} else {
+
+			dbInfo = fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable host=%s port=%s",
+				dbUser, dbPass, dbName, dbHost, dbPort)
+		}
+
+		dbpoolConn, err := sql.Open("postgres", dbInfo)
+
+		dbpoolConn.SetMaxOpenConns(Conf.Postgresxl.MaxOpenConns)
+		dbpoolConn.SetMaxIdleConns(Conf.Postgresxl.MaxIdleConns)
+		dbpoolConn.SetConnMaxLifetime(time.Duration(Conf.Postgresxl.ConnMaxLifetime) * time.Second)
+
+		if err != nil {
+
+			logger.Error(fmt.Sprintf("ErrorType : INFRA_ERROR, Not able to connect to DB Server : %v , Got error : %v", dbHost, err.Error()))
+			continue
+		} else {
+
+			logger.Info(fmt.Sprintf("Adding connection to pool, Server : %v ,Port : %v", dbHost, dbPort))
+			dbpool = append(dbpool, dbpoolConn)
+		}
+	}
+
+}
--- stream.go
+++ stream.go
@@ -5,16 +5,17 @@ import (
 	"fmt"
 	"github.com/dminGod/Stream/app_config"
 	"github.com/antigloss/go/logger"
+	"time"
 )
 
 var Conf app_config.AppConfig
 
-	// TODO: - Change the fmt.Println to proper logging [done]
-	// TODO: - Use the same logging from D30 if needed, its customized as per the requirement [done]
-	// TODO: - Write tests for all the main functions
-	// TODO: - Govendor the application so the libs are all integrated into the applicatoin [done]
-	// TODO: - The applicatoin needs to have a graceful shutdown happen so it can complete whatever its doing and then exit
-	// TODO: - All the os.Exit(1) that is used everywhere is fine for now, but if 2 threads are running on the same binary then that will get messed up?
+// TODO: - Change the fmt.Println to proper logging [done]
+// TODO: - Use the same logging from D30 if needed, its customized as per the requirement [done]
+// TODO: - Write tests for all the main functions
+// TODO: - Govendor the application so the libs are all integrated into the applicatoin [done]
+// TODO: - The applicatoin needs to have a graceful shutdown happen so it can complete whatever its doing and then exit
+// TODO: - All the os.Exit(1) that is used everywhere is fine for now, but if 2 threads are running on the same binary then that will get messed up?
 
 func main() {
 
@@ -35,17 +36,35 @@ func main() {
 		20,  // number of logfiles to delete when number of logfiles exceeds the configured limit
 		50,     // maximum size of a logfile in MB
 		false, // whether logs with Trace level are written down
-		)
+	)
 	if err != nil {
 		fmt.Println("Error in intializing logger, is : ", err)
 	}
 
-	// Get the flag for hdfs_push mode
+	var postMode bool
+
+	//Get the flag for hdfs_push mode
 	var pushMode bool
 	flag.BoolVar(&pushMode, "hdfs_push", false, `--hdfs_push Will start the binary in a push mode`)
+	//flag.Parse()
+
+	flag.BoolVar(&postMode, "post_push", false, `--post_push Will start the binary in a postgres push mode`)
 	flag.Parse()
 
-	if pushMode == false {
+	if postMode == true{
+		logger.SetFilenamePrefix("post.%P.%U", "post.%P.%U")
+		logger.Info("Starting postgres push from stream")
+		LoadPostgres()
+		time.Sleep(5 * time.Second)
+		PostgresPush()
+
+	} else if pushMode == true {
+		logger.SetFilenamePrefix("cass.%P.%U", "cass.%P.%U")
+
+		logger.Info("Starting HDFS push from stream")
+		HdfsPush()
+
+	} else {
 
 		logger.SetFilenamePrefix("hdfs.%P.%U", "hdfs.%P.%U")
 
@@ -58,13 +77,6 @@ func main() {
 		if err != nil {
 			logger.Error(fmt.Sprintf("There was an error running the listener: %v", err))
 		}
-	} else {
-
-		logger.SetFilenamePrefix("cass.%P.%U", "cass.%P.%U")
-
-		logger.Info("Starting HDFS push from stream")
-		HdfsPush()
 	}
-
 	logger.Info("Exiting application")
 }
